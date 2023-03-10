9ed179bc107e3e5f9c241b0a6656e72134e38b4b
Ka Long Ngai <kls8162@gmail.com>
2023-03-09 22:38:19 -0500

    (03/09/23)

---
+++ 382.java
@@ -0,0 +1,46 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    private ListNode head;
+
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int scope = 1, chosenValue = 0;
+        ListNode curr = this.head;
+        while (curr != null) {
+            // decide whether to include the element in reservoir
+            if (Math.random() < 1.0 / scope)
+                chosenValue = curr.val;
+            // move on to the next node
+            scope += 1;
+            curr = curr.next;
+        }
+        return chosenValue;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
