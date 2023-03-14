56aeb46ddc7fe72b3b62a1851e52018f50ef1804
Ka Long Ngai <kls8162@gmail.com>
2023-03-13 20:43:14 -0400

    (03/14/23)

---
+++ 129.java
@@ -0,0 +1,39 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return sumHelper(root, 0);
+    }
+
+    public int sumHelper(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = 10 * sum + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return sumHelper(root.left, sum) + sumHelper(root.right, sum);
+    }
+}
