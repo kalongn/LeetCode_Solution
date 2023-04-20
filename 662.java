a0be2491e91c53da064e6a9c4369a5cb4c819287
Ka Long Ngai <kls8162@gmail.com>
2023-04-19 20:31:08 -0400

    (04/20/23)

---
+++ 662.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+       if (root == null) {
+           return 0;
+       }
+        return helper(root, 0, 1, new ArrayList<Integer>());
+
+    }
+    private int helper(TreeNode root, int depth, int index, List<Integer> list) {
+        if (root == null) {
+            return 0;
+        }
+		//add index of leftmost node to list, at depth'th position in list
+        if (depth == list.size()) {
+            list.add(index);
+        }
+        int currWidth = index - list.get(depth) + 1;
+        int leftWidth = helper(root.left, depth + 1, index * 2, list);
+        int rightWidth = helper(root.right, depth + 1, index * 2 + 1, list);
+        return Math.max(currWidth, Math.max(leftWidth, rightWidth));
+    }
+}
