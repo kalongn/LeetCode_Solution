95caa4f64ef05f4e6a4fd1ad22af79bd03b612df
Ka Long Ngai <kls8162@gmail.com>
2023-05-07 20:30:47 -0400

    (05/08/23)_v2

--- 1572.java
+++ 1572.java
@@ -2,12 +2,8 @@ class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
         for (int i = 0; i < mat.length; i++) {
-            if (i != mat.length - 1 - i) {
-                sum += mat[i][i] + mat[i][mat.length - 1 - i];
-            } else {
-                sum += mat[i][i];
-            }
+            sum += mat[i][i] + mat[i][mat.length - 1 - i];
         }
-        return sum;
+        return mat.length % 2 == 0 ? sum : sum - mat[mat.length / 2][mat.length / 2];
     }
 }
