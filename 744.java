bdf6fdd1ec8048f12f0f954da9007a853f10c0a7
Ka Long Ngai <kls8162@gmail.com>
2023-06-08 21:41:53 -0400

    (06/09/23) V2

--- 744.java
+++ 744.java
@@ -1,8 +1,8 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        for (int i = 0; i < letters.length; i++) {
-            if ((int) letters[i] - (int) target > 0) {
-                return letters[i];
+        for (char i : letters) {
+            if (i > target) {
+                return i;
             }
         }
         return letters[0];
