1baceb23c42c3793825330329ec02f76fb8f92f7
Ka Long Ngai <kls8162@gmail.com>
2023-07-09 20:31:23 -0400

    (07/10/23)

---
+++ 111.java
@@ -0,0 +1,42 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return helper(root);
+    }
+
+    private int helper(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int min = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min = 1 + helper(root.left);
+        }
+        if (root.right != null) {
+            min = Math.min(min, 1 + helper(root.right));
+        }
+        return min;
+    }
+}
