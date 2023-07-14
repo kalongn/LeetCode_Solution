26bc08b3a4f1f52f0c02aafc9a964cffc34c50c5
Ka Long Ngai <kls8162@gmail.com>
2023-07-13 20:16:06 -0400

    (07/14/23)

---
+++ 1218.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestSubsequence(int[] arr, int difference) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        int answer = 1;
+
+        for (int a : arr) {
+            int beforeA = dp.getOrDefault(a - difference, 0);
+            dp.put(a, beforeA + 1);
+            answer = Math.max(answer, dp.get(a));
+        }
+
+        return answer;
+    }
+}
