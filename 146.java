090403bafcb07e2eeb5f4a924a62978e6d69e43c
Ka Long Ngai <kls8162@gmail.com>
2023-07-17 20:43:54 -0400

    (07/18/23)

---
+++ 146.java
@@ -0,0 +1,90 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache {
+
+    Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        Node headNext = head.next;
+        head.next = node;
+        node.prev = head;
+        headNext.prev = node;
+        node.next = headNext;
+    }
+
+    class Node {
+        Node prev, next;
+        int key, value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
+
+class LRUCache2 {
+    private LinkedHashMap<Integer, Integer> map;
+    private int SIZE;
+
+    public LRUCache2(int capacity) {
+        map = new LinkedHashMap<>();
+        SIZE = capacity;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            int value = map.remove(key);
+            map.put(key, value);
+            return value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+        } else if (map.size() + 1 > SIZE) {
+            map.remove(map.keySet().iterator().next());
+        }
+        map.put(key, value);
+    }
+}
