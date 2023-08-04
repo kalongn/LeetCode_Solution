9cf96084e1cf4ea39b1c8482b952f3e2908dff6b
Ka Long Ngai <kls8162@gmail.com>
2023-08-03 20:20:43 -0400

    (08/04/23)

---
+++ 139.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
+        Set<String> dict = new HashSet<>(wordDict);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; !dp[i] && j <= n; j++) {
+                dp[i] = dp[j] & dict.contains(s.substring(i, j));
+            }
+        }
+        return dp[0];
+    }
+}
