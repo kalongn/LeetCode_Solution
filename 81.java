53c48f7f83c6424c9e9fba32d311bbf088aa9d89
Ka Long Ngai <kls8162@gmail.com>
2023-08-09 20:51:21 -0400

    (08/10/23)

---
+++ 81.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[start] < nums[mid]) {
+                if (target < nums[start] || target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (nums[start] > nums[mid]) {
+                if (target < nums[mid] || target > nums[end]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                start++;
+            }
+        }
+        return false;
+    }
+}
