0d61f4915796ec7fab5293e79f9c7e8491f12e1d
Ka Long Ngai <kls8162@gmail.com>
2023-08-13 19:51:59 -0400

    Q9 Easy Difficulty

---
+++ 9.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // String Approach
+        /*
+        String str = "" + x;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+        */
+        
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == rev || x == rev / 10);
+    }
+}
