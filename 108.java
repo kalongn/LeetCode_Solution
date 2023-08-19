18a8a357e1ae2f7af05efb9a42d13a980ded9fbc
Ka Long Ngai <kls8162@gmail.com>
2023-08-18 21:38:16 -0400

    Q108 Easy Difficulty

---
+++ 108.java
@@ -0,0 +1,36 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return createTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode createTree(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = createTree(nums, left, mid - 1);
+        root.right = createTree(nums, mid + 1, right);
+        return root;
+    }
+}
