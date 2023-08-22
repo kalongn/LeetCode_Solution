d8ed9c04ea9921228f486a929b9cddbd3939cafa
Ka Long Ngai <kls8162@gmail.com>
2023-08-21 21:07:06 -0400

    (08/22/23)

---
+++ 168.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            result.append((char) (columnNumber % 26 + 'A'));
+            columnNumber /= 26;
+        }
+        return result.reverse().toString();
+    }
+}
