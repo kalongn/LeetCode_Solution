f4f92a9413fce8ac4d6e1e267f50146849abb0ff
Ka Long Ngai <kls8162@gmail.com>
2023-08-27 21:32:48 -0400

    Q144 Easy Difficulty

---
+++ 144.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        helper(root.left, list);
+        helper(root.right, list);
+    }
+}
