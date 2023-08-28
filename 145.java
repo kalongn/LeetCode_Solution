45fa379b1a7f4eb1ac7b0c5e1c06d981a9aae9a9
Ka Long Ngai <kls8162@gmail.com>
2023-08-27 21:35:10 -0400

    Q145 Easy Difficulty

---
+++ 145.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left, list);
+        helper(root.right, list);
+        list.add(root.val);
+    }
+}
