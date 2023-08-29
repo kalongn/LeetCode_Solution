2d0710a931e702b3ccadfbf3d1f343e1515c3f70
Ka Long Ngai <kls8162@gmail.com>
2023-08-29 10:39:13 -0400

    Q160 Easy Difficulty

---
+++ 160.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
