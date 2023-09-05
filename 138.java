2113b4306bd857810793dc1bdf04539d7ed6ad53
Ka Long Ngai <kls8162@gmail.com>
2023-09-04 20:15:22 -0400

    (09/05/23)

---
+++ 138.java
@@ -0,0 +1,36 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node cursor = head, next;
+        while( cursor != null ) {
+            next = cursor.next;
+            Node copy = new Node(cursor.val);
+            cursor.next = copy;
+            copy.next = next;
+            cursor = next;
+        }
+
+        cursor = head;
+        while (cursor != null) {
+            if(cursor.random != null) {
+                cursor.next.random = cursor.random.next;
+            }
+            cursor = cursor.next.next;
+        }
+
+        cursor = head;
+        Node dummyHead = new Node(0);
+        Node copy, copyCursor = dummyHead;
+        while(cursor != null) {
+            next = cursor.next.next;
+
+            copy = cursor.next;
+            copyCursor.next = copy;
+            copyCursor = copy;
+
+            cursor.next = next;
+            cursor = next;
+        }
+
+        return dummyHead.next;
+    }
+}
