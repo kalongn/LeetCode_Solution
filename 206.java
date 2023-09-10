dc30db352091592a3d7fed07a8627f089132eaa5
Ka Long Ngai <kls8162@gmail.com>
2023-09-10 16:57:50 -0400

    Q206 Easy Difficulty

---
+++ 206.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
