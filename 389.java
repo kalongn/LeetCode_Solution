4769ea1296ef413a686db6442ff3660a013a752e
Ka Long Ngai <kls8162@gmail.com>
2023-09-25 08:39:25 -0400

    (09/25/23)

---
+++ 389.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum = 0;
+        for (int i = 0; i < t.length(); i++) {
+            sum += t.charAt(i);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            sum -= s.charAt(i);
+        }
+        return (char) sum;
+    }
+}
