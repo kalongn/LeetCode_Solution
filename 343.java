2d24e04fda8dfbe9964ab124543a7195826daaf2
Ka Long Ngai <kls8162@gmail.com>
2023-10-06 14:12:52 -0400

    Modified: (10/06/23)

--- 343.java
+++ 343.java
@@ -3,7 +3,7 @@ class Solution {
         if (n == 2) {
             return 1;
         } else if (n == 3) {
-            return 1;
+            return 2;
         } else if (n % 3 == 0) {
             return (int) Math.pow(3, n / 3);
         } else if (n % 3 == 1) {
