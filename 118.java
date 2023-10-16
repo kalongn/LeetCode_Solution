73e556101b68fe9013409f62621df9e631b4a0e8
Ka Long Ngai <kls8162@gmail.com>
2023-10-15 20:13:36 -0400

    (10/16/23)

--- 118.java
+++ 118.java
@@ -15,3 +15,17 @@ class Solution {
 
     }
 }
+/*
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < rowIndex + 1; i++) {
+            answer.add(1);
+            for (int j = i - 1; j > 0; j--) {
+                answer.set(j, answer.get(j - 1) + answer.get(j));
+            }
+        }
+        return answer;
+    }
+}
+*/
