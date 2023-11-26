a4e8bdf376e22aa8bb7753678cf7513f06bf8824
Ka Long Ngai <kls8162@gmail.com>
2023-11-26 18:02:01 -0500

    (11/26/23) 2

--- 1727.java
+++ 1727.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int ans = 0;
-
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (matrix[row][col] != 0 && row > 0) {
-                    matrix[row][col] += matrix[row - 1][col];
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = matrix[i - 1][j] + 1;
                 }
             }
-
-            int[] currRow = matrix[row].clone();
-            Arrays.sort(currRow);
-            for (int i = 0; i < n; i++) {
-                ans = Math.max(ans, currRow[i] * (n - i));
+        }
+        int ans = 0;
+        for (int[] row : matrix) {
+            Arrays.sort(row);
+            for (int j = n - 1, k = 1; j >= 0 && row[j] > 0; --j, ++k) {
+                int s = row[j] * k;
+                ans = Math.max(ans, s);
             }
         }
-
         return ans;
     }
 }
