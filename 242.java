273d37c9e3cef8ef3b1e9aae5ddd314bd3399744
Ka Long Ngai <kls8162@gmail.com>
2023-12-15 19:56:22 -0500

    (12/16/23)

--- 242.java
+++ 242.java
@@ -17,3 +17,21 @@ class Solution {
         return true;
     }
 }
+
+class Solution2 {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        for (char i : s.toCharArray()) {
+            count[i - 'a']++;
+        }
+        for (char i : t.toCharArray()) {
+            count[i - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
