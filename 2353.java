baad866f050550f49b730c15fbb98a494a8d8f82
Ka Long Ngai <kls8162@gmail.com>
2023-12-16 19:18:09 -0500

    (12/17/23)

---
+++ 2353.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+class Food {
+    int rating;
+    String name, cuisine;
+
+    Food(String name, String cuisine, int rating) {
+        this.name = name;
+        this.rating = rating;
+        this.cuisine = cuisine;
+    }
+}
+
+class FoodRatings {
+    HashMap<String, PriorityQueue<Food>> x = new HashMap<>();
+    HashMap<String, Food> menu = new HashMap<>();
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        for (int i = 0; i < foods.length; i++) {
+            Food curr = new Food(foods[i], cuisines[i], ratings[i]);
+            x.putIfAbsent(cuisines[i], new PriorityQueue<>(
+                    (a, b) -> b.rating == a.rating ? a.name.compareTo(b.name) : b.rating - a.rating));
+            PriorityQueue<Food> pq = x.get(cuisines[i]);
+            pq.add(curr);
+            menu.put(foods[i], curr);
+        }
+    }
+
+    public void changeRating(String food, int newRating) {
+        Food curr = menu.get(food);
+        PriorityQueue<Food> pq = x.get(curr.cuisine);
+        pq.remove(curr);
+        curr.rating = newRating;
+        pq.add(curr);
+    }
+
+    public String highestRated(String cuisine) {
+        return x.get(cuisine).peek().name;
+    }
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.changeRating(food,newRating);
+ * String param_2 = obj.highestRated(cuisine);
+ */
