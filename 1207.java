ca0a18110624a02d4f5d3037f34a206e9f0c93d9
Ka Long Ngai <kls8162@gmail.com>
2024-01-16 22:21:22 -0500

    (01/17/24)

---
+++ 1207.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>(map.values());
+        return set.size() == map.size();
+    }
+}
