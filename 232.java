73166848ebeefebba124dca1df965b6ecfe447eb
Ka Long Ngai <kls8162@gmail.com>
2024-01-28 20:37:43 -0500

    (01/29/24)

---
+++ 232.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class MyQueue {
+
+    Deque<Integer> input, output;
+
+    public MyQueue() {
+        input = new ArrayDeque<>();
+        output = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
