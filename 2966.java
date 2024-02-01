8d753ababa584dc41f78f809b68d653daaa2bfc4
Ka Long Ngai <kls8162@gmail.com>
2024-01-31 20:14:02 -0500

    (02/01/24)

---
+++ 2966.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[][] result = new int[nums.length / 3][3];
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            result[i / 3] = new int[] { nums[i], nums[i + 1], nums[i + 2] };
+        }
+        return result;
+    }
+}
