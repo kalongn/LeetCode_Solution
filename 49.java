0cd497efc2a0f4b08b78985035b30673c6fcc415
Ka Long Ngai <kls8162@gmail.com>
2024-02-05 19:51:15 -0500

    (02/06/24)

--- 49.java
+++ 49.java
@@ -1,22 +1,21 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<List<Integer>, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            List<Integer> charCount = new ArrayList<>(26);
-            for (int i = 0; i < 26; i++) {
-                charCount.add(0);
-            }
-            for (int i = 0; i < str.length(); i++) {
-                charCount.set(str.charAt(i) - 'a', charCount.get(str.charAt(i) - 'a') + 1);
-            }
-            List<String> value = map.getOrDefault(charCount, new ArrayList<>());
-            value.add(str);
-            map.put(charCount, value);
+        if (strs == null || strs.length == 0) {
+            return Collections.emptyList();
         }
-        return map.entrySet().stream().map(x -> x.getValue()).collect(Collectors.toList());
-        // return new ArrayList<>(map.values());
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] frequencyArr = new char[26];
+            for (int i = 0; i < s.length(); i++) {
+                frequencyArr[s.charAt(i) - 'a']++;
+            }
+            String key = new String(frequencyArr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key, tempList);
+        }
+        return new LinkedList<>(map.values());
     }
 }
