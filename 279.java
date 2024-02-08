04954ae73b470b1858b2606bd2eccbe994fc773a
Ka Long Ngai <kls8162@gmail.com>
2024-02-07 21:20:54 -0500

    (02/08/24)

---
+++ 279.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+
+            for (int j = 1; j * j <= i; j++) {
+                int square = j * j;
+                dp[i] = Math.min(dp[i], 1 + dp[i - square]);
+            }
+        }
+
+        return dp[n];
+    }
+}
