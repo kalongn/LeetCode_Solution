d898afdd1d4d25f9ac54731c5e8422ee71c22ffb
Ka Long Ngai <kls8162@gmail.com>
2024-02-11 19:42:07 -0500

    (02/12/24)

--- 169.java
+++ 169.java
@@ -1,16 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority = nums[0], count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (count == 0) {
-                count++;
-                majority = nums[i];
-            } else if (majority == nums[i]) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        return majority;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
