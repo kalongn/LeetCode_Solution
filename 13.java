85e06e3158885b2861d5c2ae4d4f6ad66fbb9195
Ka Long Ngai <kls8162@gmail.com>
2024-02-21 19:24:45 -0500

    Q13 Updated approach

--- 13.java
+++ 13.java
@@ -1,50 +1,33 @@
 class Solution {
     public int romanToInt(String s) {
+        char[] arr = s.toCharArray();
         int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
+        char before = '0';
+        for (int i = arr.length - 1; i >= 0; i--) {
+            switch (arr[i]) {
                 case 'I':
-                    sum += 1;
+                    sum += before == 'V' || before == 'X' ? -1 : 1;
                     break;
                 case 'V':
                     sum += 5;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'I') {
-                        sum -= 2;
-                    }
                     break;
                 case 'X':
-                    sum += 10;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'I') {
-                        sum -= 2;
-                    }
+                    sum += before == 'L' || before == 'C' ? -10 : 10;
                     break;
                 case 'L':
                     sum += 50;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'X') {
-                        sum -= 20;
-                    }
                     break;
                 case 'C':
-                    sum += 100;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'X') {
-                        sum -= 20;
-                    }
+                    sum += before == 'D' || before == 'M' ? -100 : 100;
                     break;
                 case 'D':
                     sum += 500;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'C') {
-                        sum -= 200;
-                    }
                     break;
                 case 'M':
                     sum += 1000;
-                    if (i - 1 >= 0 && s.charAt(i - 1) == 'C') {
-                        sum -= 200;
-                    }
-                    break;
-                default:
                     break;
             }
+            before = arr[i];
         }
         return sum;
     }
