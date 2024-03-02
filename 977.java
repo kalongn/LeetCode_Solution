530ac6fcee7f5ec5f10f9c1c470c8ab4952ec729
Ka Long Ngai <kls8162@gmail.com>
2024-03-01 19:56:00 -0500

    (03/02/24)

---
+++ 977.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = nums.length - 1;
+        int result[] = new int[nums.length];
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+            index--;
+        }
+        return result;
+    }
+}
