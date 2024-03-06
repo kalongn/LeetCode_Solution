f6c9f2221af59e7d9b4812c0525dcbee00b80a4e
Ka Long Ngai <kls8162@gmail.com>
2024-03-05 20:35:39 -0500

    (03/06/24)

--- 141.java
+++ 141.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head, fast = head;
+        ListNode fast = head, slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
