5437ea55e0b7a25ae16461e6091f3631ab0fce48
Ka Long Ngai <kls8162@gmail.com>
2024-03-06 19:23:48 -0500

    Leetcode workshop with GDSC Stony

--- 238.java
+++ 238.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
+        int ans[] = new int[n];
         Arrays.fill(ans, 1);
         int curr = 1;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             ans[i] *= curr;
             curr *= nums[i];
         }
         curr = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             ans[i] *= curr;
             curr *= nums[i];
         }
