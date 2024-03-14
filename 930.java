2e8007da7b7e9c463e38445c9006625708165ed0
Ka Long Ngai <kls8162@gmail.com>
2024-03-13 21:23:34 -0400

    (03/14/24)

---
+++ 930.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int totalCount = 0, currentSum = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum == goal) {
+                totalCount++;
+            }
+
+            if (freq.containsKey(currentSum - goal)) {
+                totalCount += freq.get(currentSum - goal);
+            }
+            freq.put(currentSum, freq.getOrDefault(currentSum, 0) + 1);
+        }
+        return totalCount;
+    }
+}
