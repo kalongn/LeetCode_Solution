56f473786dfc1e6d34891950e380506d1ab5cdbc
Ka Long Ngai <kls8162@gmail.com>
2024-03-14 21:18:02 -0400

    Q385, Easy

---
+++ 385.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i : nums2) {
+            if (map.get(i) != null && map.get(i) > 0) {
+                list.add(i);
+                map.put(i, map.get(i) - 1);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
