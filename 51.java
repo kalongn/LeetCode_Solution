b7dbdec9229eb9706435b587faa371745caf3ee6
Ka Long Ngai <kls8162@gmail.com>
2024-03-16 21:59:09 -0400

    (03/17/24)

---
+++ 51.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (newInterval[1] < interval[0]) {
+                result.add(newInterval);
+                newInterval = interval;
+            } else if (interval[1] < newInterval[0]) {
+                result.add(interval);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+        }
+        result.add(newInterval);
+        return result.toArray(new int[result.size()][]);
+    }
+}
