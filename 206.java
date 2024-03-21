1a4a647f5f45e98566d987692fe709a3b40ad307
Ka Long Ngai <kls8162@gmail.com>
2024-03-20 20:59:58 -0400

    (03/21/24)

--- 206.java
+++ 206.java
@@ -10,3 +10,18 @@ class Solution {
         return newHead;
     }
 }
+
+class Solution2 {
+    public ListNode reverseList(ListNode head) {
+        return helper(head, null);
+    }
+
+    private ListNode helper(ListNode head, ListNode newHead) {
+        if (head == null) {
+            return newHead;
+        }
+        ListNode next = head.next;
+        head.next = newHead;
+        return helper(next, head);
+    }
+}
