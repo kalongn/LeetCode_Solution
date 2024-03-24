9463b745c5478d518912d1c770160f608bd8c897
Ka Long Ngai <kls8162@gmail.com>
2024-03-23 21:32:28 -0400

    (03/24/24)

--- 287.java
+++ 287.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = 0, fast = 0;
+        int slow = nums[0], fast = nums[0];
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
 
-        slow = 0;
+        slow = nums[0];
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
