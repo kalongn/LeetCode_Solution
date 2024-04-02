ae48db6eb5a27f7a20071c00ef56681d6574e681
Ka Long Ngai <kls8162@gmail.com>
2024-04-01 20:21:54 -0400

    (04/02/24)

--- 205.java
+++ 205.java
@@ -1,27 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s == null || s.length() <= 1) {
-            return true;
-        }
-        Map<Character, Character> map = new HashMap<>();
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        for (int i = 0; i < sArray.length; i++) {
-            if (map.containsKey(sArray[i])) {
-                if (map.get(sArray[i]).equals(tArray[i])) {
-                    continue;
-                } else {
-                    return false;
-                }
-            } else {
-                if (!map.containsValue(tArray[i])) {
-                    map.put(sArray[i], tArray[i]);
-                } else {
-                    return false;
-                }
+        int mapS[] = new int[128];
+        int mapT[] = new int[128];
+
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (mapS[s.charAt(i)] != mapT[t.charAt(i)]) {
+                return false;
             }
+            mapS[s.charAt(i)] = i + 1;
+            mapT[t.charAt(i)] = i + 1;
         }
         return true;
     }
