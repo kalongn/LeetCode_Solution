e693d67488d691628770a230699cfdccbcf16988
Ka Long Ngai <kls8162@gmail.com>
2024-04-14 20:50:54 -0400

    (04/15/24)

--- 129.java
+++ 129.java
@@ -1,39 +1,16 @@
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
     public int sumNumbers(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return sumHelper(root, 0);
+        return helper(root, 0);
     }
 
-    public int sumHelper(TreeNode root, int sum) {
+    private int helper(TreeNode root, int sum) {
         if (root == null) {
             return 0;
         }
-        sum = 10 * sum + root.val;
+        sum = sum * 10 + root.val;
         if (root.left == null && root.right == null) {
             return sum;
         }
-        return sumHelper(root.left, sum) + sumHelper(root.right, sum);
+        return helper(root.left, sum) + helper(root.right, sum);
     }
 }
