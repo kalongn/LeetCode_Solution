ac2b87022eeee588830059b2edd0a3968e4919f1
Ka Long Ngai <kls8162@gmail.com>
2024-06-29 20:44:03 -0400

    (06/30/24)

--- 1579.java
+++ 1579.java
@@ -1,88 +1,73 @@
-class Solution {
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
-        // Different objects for Alice and Bob.
-        UnionFind Alice = new UnionFind(n);
-        UnionFind Bob = new UnionFind(n);
+import java.util.Arrays;
 
-        int edgesRequired = 0;
-        // Perform union for edges of type = 3, for both Alice and Bob.
-        for (int[] edge : edges) {
-            if (edge[0] == 3) {
-                edgesRequired += (Alice.performUnion(edge[1], edge[2]) | Bob.performUnion(edge[1], edge[2]));
-            }
+class DSU {
+    int[] parent;
+    int[] rank;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-
-        // Perform union for Alice if type = 1 and for Bob if type = 2.
-        for (int[] edge : edges) {
-            if (edge[0] == 1) {
-                edgesRequired += Alice.performUnion(edge[1], edge[2]);
-            } else if (edge[0] == 2) {
-                edgesRequired += Bob.performUnion(edge[1], edge[2]);
-            }
-        }
-
-        // Check if the Graphs for Alice and Bob have n - 1 edges or is a single
-        // component.
-        if (Alice.isConnected() && Bob.isConnected()) {
-            return edges.length - edgesRequired;
-        }
-
-        return -1;
     }
 
-    private class UnionFind {
-        int[] representative;
-        int[] componentSize;
-        // Number of distinct components in the graph.
-        int components;
-
-        // Initialize the list representative and componentSize
-        // Each node is representative of itself with size 1.
-        public UnionFind(int n) {
-            components = n;
-            representative = new int[n + 1];
-            componentSize = new int[n + 1];
-
-            for (int i = 0; i <= n; i++) {
-                representative[i] = i;
-                componentSize[i] = 1;
-            }
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
         }
+        return parent[x];
+    }
 
-        // Get the root of a node.
-        int findRepresentative(int x) {
-            if (representative[x] == x) {
-                return x;
-            }
-
-            // Path compression.
-            return representative[x] = findRepresentative(representative[x]);
-        }
-
-        // Perform the union of two components that belongs to node x and node y.
-        int performUnion(int x, int y) {
-            x = findRepresentative(x);
-            y = findRepresentative(y);
-
-            if (x == y) {
-                return 0;
-            }
-
-            if (componentSize[x] > componentSize[y]) {
-                componentSize[x] += componentSize[y];
-                representative[y] = x;
+    public boolean union(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset != yset) {
+            if (rank[xset] < rank[yset]) {
+                parent[xset] = yset;
+            } else if (rank[xset] > rank[yset]) {
+                parent[yset] = xset;
             } else {
-                componentSize[y] += componentSize[x];
-                representative[x] = y;
+                parent[xset] = yset;
+                rank[yset]++;
             }
+            return true;
+        }
+        return false;
+    }
+}
 
-            components--;
-            return 1;
+class Solution {
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        Arrays.sort(edges, (a, b) -> Integer.compare(b[0], a[0]));
+
+        DSU dsu_alice = new DSU(n + 1);
+        DSU dsu_bob = new DSU(n + 1);
+
+        int removed_edge = 0, alice_edges = 0, bob_edges = 0;
+        for (int[] edge : edges) {
+            if (edge[0] == 3) {
+                if (dsu_alice.union(edge[1], edge[2])) {
+                    dsu_bob.union(edge[1], edge[2]);
+                    alice_edges++;
+                    bob_edges++;
+                } else {
+                    removed_edge++;
+                }
+            } else if (edge[0] == 2) {
+                if (dsu_bob.union(edge[1], edge[2])) {
+                    bob_edges++;
+                } else {
+                    removed_edge++;
+                }
+            } else {
+                if (dsu_alice.union(edge[1], edge[2])) {
+                    alice_edges++;
+                } else {
+                    removed_edge++;
+                }
+            }
         }
 
-        // Returns true if all nodes get merged to one.
-        boolean isConnected() {
-            return components == 1;
-        }
+        return (bob_edges == n - 1 && alice_edges == n - 1) ? removed_edge : -1;
     }
 }
