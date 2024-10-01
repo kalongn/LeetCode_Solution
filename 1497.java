1cab843298be97b83ba815be5b5ba992bc9c7e3b
Ka Long Ngai <kls8162@gmail.com>
2024-09-30 21:28:50 -0400

    (09/30/24)

---
+++ 1497.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] freq = new int[k];
+        for (int num : arr) {
+            num = ((num % k) + k) % k;
+            freq[num]++;
+        }
+
+        if (freq[0] % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
