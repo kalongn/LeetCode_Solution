ca403bd8f8afde4c55847a0e1280c94131ffa9b9
Ka Long Ngai <kls8162@gmail.com>
2024-11-01 20:32:41 -0400

    (11/01/24)

---
+++ 2490.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length - 1; i++) {
+            if (words[i].charAt(words[i].length() - 1) != words[i + 1].charAt(0)) {
+                return false;
+            }
+        }
+        return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
+    }
+}
