baff7d90849b27328c414ea9bfe4b1e7006f741d
Ka Long Ngai <kls8162@gmail.com>
2024-12-10 23:44:41 -0500

    (12/10/24) P4

--- 33.java
+++ 33.java
@@ -1,37 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int left = 0, right = n - 1;
-
-        // Find the index of the pivot element (the smallest element)
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[n - 1]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-        }
-
-        return shiftedBinarySearch(nums, left, target);
-    }
-
-    // Shift elements in a circular manner, with the pivot element at index 0.
-    // Then perform a regular binary search
-    private int shiftedBinarySearch(int[] nums, int pivot, int target) {
-        int n = nums.length;
-        int shift = n - pivot;
-        int left = (pivot + shift) % n;
-        int right = (pivot - 1 + shift) % n;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[(mid - shift + n) % n] == target) {
-                return (mid - shift + n) % n;
-            } else if (nums[(mid - shift + n) % n] > target) {
-                right = mid - 1;
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                left = mid + 1;
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
