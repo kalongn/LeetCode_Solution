932a782bcd25d89fe88fa3d8a95d7772297d81ec
Ka Long Ngai <kls8162@gmail.com>
2024-12-19 22:59:19 -0500

    (12/19/24)

---
+++ 2415.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> nodes = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                nodes.add(node);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            if (level % 2 == 1) {
+                int left = 0, right = nodes.size() - 1;
+                while (left < right) {
+                    int temp = nodes.get(left).val;
+                    nodes.get(left).val = nodes.get(right).val;
+                    nodes.get(right).val = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            level++;
+        }
+        return root;
+    }
+}
