c0ba6b52c818a822c21661e29f78e426447d3b6b
Ka Long Ngai <kls8162@gmail.com>
2024-12-31 20:43:53 -0500

    (12/31/24)

--- 1422.java
+++ 1422.java
@@ -1,23 +1,22 @@
 class Solution {
     public int maxScore(String s) {
-        int ones = 0;
-        int zeros = 0;
-        int best = Integer.MIN_VALUE;
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '1') {
+        int result = 0;
+        int zeros = 0, ones = 0;
+        char[] arr = s.toCharArray();
+        for(char i : arr) {
+            if(i == '1') {
                 ones++;
-            } else {
-                zeros++;
             }
-
-            best = Math.max(best, zeros - ones);
         }
 
-        if (s.charAt(s.length() - 1) == '1') {
-            ones++;
+        for(int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] == '0') {
+                zeros++;
+            } else {
+                ones--;
+            }
+            result = Math.max(result, zeros + ones);
         }
-
-        return best + ones;
+        return result;
     }
 }
