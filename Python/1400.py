c1d0a6e12863717fa9579cbfebb933a8294ce9be
Ka Long Ngai <kls8162@gmail.com>
2025-01-10 19:43:41 -0500

    (01/10/25) one-liner

--- Python/1400.py
+++ Python/1400.py
@@ -3,8 +3,7 @@ from typing import Counter
 
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        freq = Counter(s)
-
-        amt_odd = sum(1 for count in freq.values() if count % 2 == 1)
-
-        return amt_odd <= k and len(s) >= k
+        return (
+            len(s) >= k
+            and sum(1 for count in Counter(s).values() if count % 2 == 1) <= k
+        )
