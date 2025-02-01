a9f091153dd0fbc9b769563fb63e261879476db4
Ka Long Ngai <kls8162@gmail.com>
2025-01-31 21:34:04 -0500

    (01/31/25)

---
+++ Python/3151.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if nums[i] % 2 == nums[i + 1] % 2:
+                return False
+
+        return True
