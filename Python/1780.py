282fb772e47b217327770656673dc911b46ef7ef
Ka Long Ngai <kls8162@gmail.com>
2025-03-03 21:48:13 -0500

    (03/03/25)

---
+++ Python/1780.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        power = 0
+
+        while 3**power <= n:
+            power += 1
+
+        while n > 0:
+            if n >= 3**power:
+                n -= 3**power
+
+            if n >= 3**power:
+                return False
+
+            power -= 1
+
+        return True
