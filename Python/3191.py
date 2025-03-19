27f42e851543df4b99418e048397b75cdd770e01
Ka Long Ngai <kls8162@gmail.com>
2025-03-18 22:55:36 -0400

    (03/18/25)

---
+++ Python/3191.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n - 2):
+            if nums[i] == 0:
+                nums[i] = 1
+                nums[i + 1] = 1 if nums[i + 1] == 0 else 0
+                nums[i + 2] = 1 if nums[i + 2] == 0 else 0
+                count += 1
+
+        if nums[n - 2] == 0 or nums[n - 1] == 0:
+            return -1
+        return count
