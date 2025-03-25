503f702b14ea3539c70ff7b8d506ad999cec3b25
Ka Long Ngai <kls8162@gmail.com>
2025-03-25 00:18:38 -0400

    (03/23/25)

---
+++ Python/3169.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countDays(self, days: int, meetings: list[list[int]]) -> int:
+        free_days = 0
+        latest_end = 0
+
+        # Sort meetings based on starting times
+        meetings.sort()
+
+        for start, end in meetings:
+            # Add current range of days without a meeting
+            if start > latest_end + 1:
+                free_days += start - latest_end - 1
+
+            # Update latest meeting end
+            latest_end = max(latest_end, end)
+
+        # Add all days after the last day of meetings
+        free_days += days - latest_end
+
+        return free_days
