63f29d9809c66e4e347b926ecc276a8dd0859cd8
Ka Long Ngai <kls8162@gmail.com>
2025-04-02 23:00:28 -0400

    (04/02/25)

---
+++ Python/2874.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        res, imax, dmax = 0, 0, 0
+        for k in range(n):
+            res = max(res, dmax * nums[k])
+            dmax = max(dmax, imax - nums[k])
+            imax = max(imax, nums[k])
+        return res
