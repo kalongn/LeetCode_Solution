2223273a9c26662e53483fe9722ad29f1f4d7001
Ka Long Ngai <kls8162@gmail.com>
2025-04-08 20:06:42 -0400

    (04/08/25)

---
+++ Python/3375.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        seen = set()
+        for num in nums:
+            if num < k:
+                return -1
+            elif num > k:
+                seen.add(num)
+        return len(seen)
