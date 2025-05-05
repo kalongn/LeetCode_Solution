42cd952d6bcd06bb640901a88e7563e4a98d6100
Ka Long Ngai <kls8162@gmail.com>
2025-05-04 20:26:47 -0400

    (05/04/25)

---
+++ Python/790.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = [1, 2, 5] + [0] * n
+        for i in range(3, n):
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007
+        return dp[n - 1]
