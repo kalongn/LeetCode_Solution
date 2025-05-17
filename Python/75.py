f5dcd4c2737b0aa4304e5836af21e63427fd10cd
Ka Long Ngai <kls8162@gmail.com>
2025-05-16 22:28:23 -0400

    (05/15/25)

--- Python/75.py
+++ Python/75.py
@@ -3,15 +3,15 @@ from typing import List
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        freq = [0] * 3
-        for num in nums:
-            freq[num] += 1
+        red, white, blue = 0, 0, len(nums) - 1
 
-        index = 0
-        for i, f in enumerate(freq):
-            for _ in range(f):
-                nums[index] = i
-                index += 1
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
