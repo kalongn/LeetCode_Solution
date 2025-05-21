84ce96fb79c4270b80980cd7962de9b355d7789a
Ka Long Ngai <kls8162@gmail.com>
2025-05-20 21:13:19 -0700

    (05/20/25) replacement code.

---
+++ Python/290.py
@@ -0,0 +1,9 @@
+from itertools import zip_longest
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        s = s.split()
+
+        return len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s)))
