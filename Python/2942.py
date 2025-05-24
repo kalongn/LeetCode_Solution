a39225186daf7963bda52c7f7c2ca72f6561935f
Ka Long Ngai <kls8162@gmail.com>
2025-05-23 20:41:55 -0700

    (05/23/25)

---
+++ Python/2942.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result = []
+        for i, word in enumerate(words):
+            if x in word:
+                result.append(i)
+        return result
