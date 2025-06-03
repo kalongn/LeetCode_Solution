96854a2fc0e44f9b6826a6a58b78b92cfc731f93
Ka Long Ngai <kls8162@gmail.com>
2025-06-02 20:42:15 -0700

    (06/02/25)

---
+++ Python/1298.py
@@ -0,0 +1,42 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def maxCandies(
+        self,
+        status: List[int],
+        candies: List[int],
+        keys: List[List[int]],
+        containedBoxes: List[List[int]],
+        initialBoxes: List[int],
+    ) -> int:
+        n = len(status)
+        can_open = [status[i] == 1 for i in range(n)]
+        has_box, used = [False] * n, [False] * n
+
+        q = deque()
+        ans = 0
+        for box in initialBoxes:
+            has_box[box] = True
+            if can_open[box]:
+                q.append(box)
+                used[box] = True
+                ans += candies[box]
+
+        while len(q) > 0:
+            big_box = q.popleft()
+            for key in keys[big_box]:
+                can_open[key] = True
+                if not used[key] and has_box[key]:
+                    q.append(key)
+                    used[key] = True
+                    ans += candies[key]
+            for box in containedBoxes[big_box]:
+                has_box[box] = True
+                if not used[box] and can_open[box]:
+                    q.append(box)
+                    used[box] = True
+                    ans += candies[box]
+
+        return ans
