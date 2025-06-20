03219afe75a96d7b8152592996c148ef51c699dc
Ka Long Ngai <kls8162@gmail.com>
2025-06-19 19:59:23 -0700

    (06/19/25)

---
+++ Python/3443.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        latitude = 0
+        longitude = 0
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            if s[i] == "N":
+                latitude += 1
+            elif s[i] == "S":
+                latitude -= 1
+            elif s[i] == "E":
+                longitude += 1
+            elif s[i] == "W":
+                longitude -= 1
+            ans = max(ans, min(abs(latitude) + abs(longitude) + k * 2, i + 1))
+        return ans
