9fc4fb9d28971e52a3c47d10f045df94965e673a
Ka Long Ngai <kls8162@gmail.com>
2025-06-29 23:37:05 -0700

    (06/29/25)

---
+++ Python/594.py
@@ -0,0 +1,15 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        frequency_map = Counter(nums)
+        max_length = 0
+
+        for num in frequency_map:
+            if num + 1 in frequency_map:
+                current_length = frequency_map[num] + frequency_map[num + 1]
+                max_length = max(max_length, current_length)
+
+        return max_length
