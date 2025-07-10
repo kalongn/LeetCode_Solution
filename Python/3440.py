6a9d8da7a0abd6d09ca1e3ecd32eb882736707a8
Ka Long Ngai <kls8162@gmail.com>
2025-07-09 20:56:07 -0700

    (07/09/25)

---
+++ Python/3440.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxFreeTime(
+        self, eventTime: int, startTime: list[int], endTime: list[int]
+    ) -> int:
+        n = len(startTime)
+        q = [False] * n
+        t1 = 0
+        t2 = 0
+        for i in range(n):
+            if endTime[i] - startTime[i] <= t1:
+                q[i] = True
+            t1 = max(t1, startTime[i] - (0 if i == 0 else endTime[i - 1]))
+
+            if endTime[n - i - 1] - startTime[n - i - 1] <= t2:
+                q[n - i - 1] = True
+            t2 = max(
+                t2,
+                (eventTime if i == 0 else startTime[n - i]) - endTime[n - i - 1],
+            )
+
+        res = 0
+        for i in range(n):
+            left = 0 if i == 0 else endTime[i - 1]
+            right = eventTime if i == n - 1 else startTime[i + 1]
+            if q[i]:
+                res = max(res, right - left)
+            else:
+                res = max(res, right - left - (endTime[i] - startTime[i]))
+        return res
