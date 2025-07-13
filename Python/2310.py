3be8cd147718e0479b85abde635507d8099285c8
Ka Long Ngai <kls8162@gmail.com>
2025-07-13 09:57:24 -0700

    (07/12/25)

---
+++ Python/2310.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        m, n = len(players), len(trainers)
+        i = j = count = 0
+
+        while i < m and j < n:
+            while j < n and players[i] > trainers[j]:
+                j += 1
+            if j < n:
+                count += 1
+            i += 1
+            j += 1
+
+        return count
