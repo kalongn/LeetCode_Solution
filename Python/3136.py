0bb5670de5d13db292da357f38dc9f27f8dbd69e
Ka Long Ngai <kls8162@gmail.com>
2025-07-14 21:12:27 -0700

    (07/14/25)

---
+++ Python/3136.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        has_vowel = False
+        has_consonant = False
+
+        for c in word:
+            if c.isalpha():
+                if c.lower() in "aeiou":
+                    has_vowel = True
+                else:
+                    has_consonant = True
+            elif not c.isdigit():
+                return False
+
+        return has_vowel and has_consonant
