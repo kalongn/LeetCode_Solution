33746406ed551ba044ca0412d1ce649953d76932
Ka Long Ngai <kls8162@gmail.com>
2025-07-17 22:01:56 -0700

    (07/17/25)

---
+++ Python/2163.py
@@ -0,0 +1,34 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        n = len(nums) // 3
+
+        part1 = [0] * (n + 1)
+        # max heap
+        total = sum(nums[:n])
+        ql = [-x for x in nums[:n]]
+        heapq.heapify(ql)
+        part1[0] = total
+
+        for i in range(n, n * 2):
+            total += nums[i]
+            heapq.heappush(ql, -nums[i])
+            total -= -heapq.heappop(ql)
+            part1[i - (n - 1)] = total
+
+        # min heap
+        part2 = sum(nums[n * 2 :])
+        qr = nums[n * 2 :]
+        heapq.heapify(qr)
+        result = part1[n] - part2
+
+        for i in range(n * 2 - 1, n - 1, -1):
+            part2 += nums[i]
+            heapq.heappush(qr, nums[i])
+            part2 -= heapq.heappop(qr)
+            result = min(result, part1[i - n] - part2)
+
+        return result
