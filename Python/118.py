162938c0ef35712542d57e946bee16c347f94e61
Ka Long Ngai <kls8162@gmail.com>
2025-07-31 23:15:39 -0700

    (07/31/25)

---
+++ Python/118.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        if numRows == 0:
+            return result
+
+        first_row = [1]
+        result.append(first_row)
+
+        for i in range(1, numRows):
+            prev_row = result[i - 1]
+            current_row = [1]
+
+            for j in range(1, i):
+                current_row.append(prev_row[j - 1] + prev_row[j])
+
+            current_row.append(1)
+            result.append(current_row)
+
+        return result
