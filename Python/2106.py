a8c18ef739afb04088fdc5395c1334704eae827a
Ka Long Ngai <kls8162@gmail.com>
2025-08-02 21:17:13 -0700

    (08/02/25)

---
+++ Python/2106.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
+        left = 0
+        right = 0
+        n = len(fruits)
+        sum = 0
+        ans = 0
+
+        def step(left: int, right: int) -> int:
+            if fruits[right][0] <= startPos:
+                return startPos - fruits[left][0]
+            elif fruits[left][0] >= startPos:
+                return fruits[right][0] - startPos
+            else:
+                return (
+                    min(
+                        abs(startPos - fruits[right][0]),
+                        abs(startPos - fruits[left][0]),
+                    )
+                    + fruits[right][0]
+                    - fruits[left][0]
+                )
+
+        # each time fix the right boundary of the window
+        while right < n:
+            sum += fruits[right][1]
+            # move left boundary
+            while left <= right and step(left, right) > k:
+                sum -= fruits[left][1]
+                left += 1
+
+            ans = max(ans, sum)
+            right += 1
+
+        return ans
