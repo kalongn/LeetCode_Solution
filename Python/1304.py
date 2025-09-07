ea3e5e08573293e9a415854c1a9236cb4210c287
Ka Long Ngai <kls8162@gmail.com>
2025-09-06 20:53:35 -0400

    (09/06/25)

---
+++ Python/1304.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        result = []
+        for i in range(1, n // 2 + 1):
+            result.append(i)
+            result.append(-i)
+        
+        if n % 2 == 1:
+            result.append(0)
+        return result
