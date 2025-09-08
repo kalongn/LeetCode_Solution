0c72afcc4d468395ea9c4126f8494f5b645aacb3
Ka Long Ngai <kls8162@gmail.com>
2025-09-07 20:21:27 -0400

    (09/07/25)

---
+++ Python/1317.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for a in range(1, n):
+            b = n - a
+            if "0" not in str(a) + str(b):
+                return [a, b]
+        return []
