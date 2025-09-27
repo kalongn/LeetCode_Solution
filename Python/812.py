eaedecca24f0d41c116a633f853be3e417129abc
Ka Long Ngai <kls8162@gmail.com>
2025-09-26 20:03:19 -0400

    (09/26/25)

---
+++ Python/812.py
@@ -0,0 +1,16 @@
+import itertools
+
+
+class Solution(object):
+    def largestTriangleArea(self, points):
+        def area(p, q, r):
+            return 0.5 * abs(
+                p[0] * q[1]
+                + q[0] * r[1]
+                + r[0] * p[1]
+                - p[1] * q[0]
+                - q[1] * r[0]
+                - r[1] * p[0]
+            )
+
+        return max(area(*triangle) for triangle in itertools.combinations(points, 3))
