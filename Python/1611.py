e40f5d1f432349afd8648088e7730abe33f8fbb3
Ka Long Ngai <kls8162@gmail.com>
2025-11-08 15:09:08 -0500

    (11/07/25)

---
+++ Python/1611.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        ans = 0
+        k = 0
+        mask = 1
+
+        while mask <= n:
+            if n & mask:
+                ans = 2 ** (k + 1) - 1 - ans
+
+            mask <<= 1
+            k += 1
+
+        return ans
