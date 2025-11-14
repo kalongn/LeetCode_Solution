79d9d419bc4313152db8beefa46a79e4dbd8bd9c
Ka Long Ngai <kls8162@gmail.com>
2025-11-13 19:26:30 -0500

    (11/13/25)

---
+++ Python/2536.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        result = [[0] * n for _ in range(n)]
+
+        for r1, c1, r2, c2 in queries:
+            result[r1][c1] += 1
+            if r2 + 1 < n:
+                result[r2 + 1][c1] -= 1
+            if c2 + 1 < n:
+                result[r1][c2 + 1] -= 1
+            if r2 + 1 < n and c2 + 1 < n:
+                result[r2 + 1][c2 + 1] += 1
+
+        for r in range(1, n):
+            for c in range(n):
+                result[r][c] += result[r - 1][c]
+
+        for r in range(n):
+            for c in range(1, n):
+                result[r][c] += result[r][c - 1]
+
+        return result
