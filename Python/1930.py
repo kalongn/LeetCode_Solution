7c2f4df49e4d9bf5fed575f09d8f034beb652e43
Ka Long Ngai <kls8162@gmail.com>
2025-11-20 19:26:20 -0800

    (11/20/25)

--- Python/1930.py
+++ Python/1930.py
@@ -1,18 +1,19 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        indices = dict()
-        for i in range(len(s)):
-            c = s[i]
-            if c not in indices:
-                indices[c] = (i, i)
-            else:
-                first = indices[c][0]
-                indices[c] = (first, i)
+        left_most = [-1] * 26
+        right_most = [-1] * 26
+
+        for i, c in enumerate(s):
+            c_index = ord(c) - ord("a")
+            if left_most[c_index] == -1:
+                left_most[c_index] = i
+            right_most[c_index] = i
 
         result = 0
-        for value in indices.values():
-            if value[0] == value[1]:
+        for c_index in range(26):
+            if left_most[c_index] == -1 or left_most[c_index] == right_most[c_index]:
                 continue
-            result += len(set(s[value[0] + 1 : value[1]]))
+
+            result += len(set(s[left_most[c_index] + 1 : right_most[c_index]]))
 
         return result
